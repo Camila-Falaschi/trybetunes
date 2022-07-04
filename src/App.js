8c1b06ec7bda0810c5aa88f34898954b11ef7e7f
Camila <falaschi.camila@yahoo.com.br>
2022-07-04 16:45:13 -0300

    requisito 1: criando componentes e rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!</p>);
+    return (
+      <main>
+        <p>TrybeTunes!</p>
+
+        <Switch>
+          <Route path="/" component={ Login } exact />
+          <Route path="/search" component={ Search } exact />
+          <Route path="/album/:id" component={ Album } exact />
+          <Route path="/favorites" component={ Favorites } exact />
+          <Route path="/profile" component={ Profile } exact />
+          <Route path="/profile/edit" component={ ProfileEdit } exact />
+          <Route path="*" component={ NotFound } exact />
+        </Switch>
+      </main>
+    );
   }
 }
 
