c89b2d6d4f2cb3195222582afcd959840aea82ad
Camila <falaschi.camila@yahoo.com.br>
2022-07-05 19:38:36 -0300

    requisito 2: formulário de login e troca de componente

---
+++ src/pages/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Loading extends React.Component {
+  render() {
+    return (
+        <div>
+            {console.log('test')}
+      <p>Carregando...</p>
+      </div>
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,85 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import logoTrybeTunes from '../images/logo_TrybeTunes.png';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      isButtonDisabled: true,
+      loading: false,
+    };
+  }
+
+  onChangeState = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => ((value.length <= 2)
+      ? (this.setState({
+        isButtonDisabled: true,
+      }))
+      : (this.setState({
+        isButtonDisabled: false,
+      }))));
+  }
+
+  // Para a função 'signInButton' foi recebido a orientação e auxilio da instrutora Samanta Below da Trybe
+  signInButton = (event) => {
+    event.preventDefault();
+    const { history } = this.props;
+    const { userName } = this.state;
+    this.setState({
+      loading: true,
+    }, async () => {
+      await createUser({ name: userName });
+      history.push('/search');
+    });
+  }
+
   render() {
+    const { userName, isButtonDisabled, loading } = this.state;
     return (
       <div data-testid="page-login">
-        <h1>Bem-vindo</h1>
+        {
+          loading ? <Loading />
+            : (
+              <>
+                <img src={ logoTrybeTunes } alt="Logo Trybe Tunes" />
+                <form>
+                  <input
+                    type="text"
+                    name="userName"
+                    placeholder="Nome"
+                    data-testid="login-name-input"
+                    value={ userName }
+                    onChange={ this.onChangeState }
+                  />
+                  <button
+                    type="submit"
+                    data-testid="login-submit-button"
+                    disabled={ isButtonDisabled }
+                    onClick={ this.signInButton }
+                  >
+                    Entrar
+                  </button>
+                </form>
+              </>
+            )
+        }
       </div>
     );
   }
 }
+
+// Para o 'PropTypes' do 'history' foi consultado o Stack OverFlow (https://stackoverflow.com/questions/52109592/react-router-the-prop-history-is-undefined)
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
