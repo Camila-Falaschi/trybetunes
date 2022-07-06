02b547a6150a6065c73a14102288bb68b369e3a1
Camila <falaschi.camila@yahoo.com.br>
2022-07-06 09:29:55 -0300

    requisito 5 adicionando barra e botão de pesquisa

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,52 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchSinger: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  onChangeState = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => ((value.length < 2)
+      ? (this.setState({
+        isButtonDisabled: true,
+      }))
+      : (this.setState({
+        isButtonDisabled: false,
+      }))));
+  };
+
   render() {
+    const { searchSinger, isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Pesquise algo (Search)</h1>
+        <section>
+          <form>
+            <input
+              type="text"
+              name="searchSinger"
+              placeholder="Nome do Artista"
+              data-testid="search-artist-input"
+              value={ searchSinger }
+              onChange={ this.onChangeState }
+            />
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ isButtonDisabled }
+              onClick={ this.searchButton }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </section>
       </div>
     );
   }
