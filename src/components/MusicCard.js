f49562a4563a6ebd5523dbdb388e01a63a576813
Camila <falaschi.camila@yahoo.com.br>
2022-07-07 11:29:36 -0300

    adicionando referência

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -14,6 +14,7 @@ class MusicCard extends React.Component {
     };
   }
 
+  // Para a condicional 'isChecked' 'removeSong()' foi recebido a orientação e auxilio do Willian Portela - Turma 22 - Tribo A
   saveAsFavorite = () => {
     const { song } = this.props;
     const { isChecked } = this.state;
